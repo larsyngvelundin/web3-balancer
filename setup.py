97c2219464086a2714b864df4e91f7db5633b404
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-10-29 23:47:03 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='web3-balancer',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/larsyngvelundin/web3-balancer/',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Lars Lundin',
+    author_email='lars.y.lundin@gmail.com',
+    description='A connection balancer for web3'
+)
