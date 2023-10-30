5b8bf5970c6914f02ba135b1318b08fd57dbff4e
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-10-30 13:00:18 +0100

    0.1 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Lars Lundin',
     author_email='your.email@domain.com',
     url='https://github.com/larsyngvelundin/web3_balancer',
-    download_url='https://github.com/larsyngvelundin/web3_balancer/releases/v_01.tar.gz',
+    download_url='https://github.com/larsyngvelundin/web3_balancer/archive/refs/tags/v.011.tar.gz',
     keywords=['web3'],
     install_requires=[
         'web3',
@@ -19,7 +19,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU GPL-3.0 license',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
     ],
