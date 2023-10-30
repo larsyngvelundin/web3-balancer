6c7b3121d8b61f3a6d2071b6cbbf519a2941fee9
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-10-30 13:36:15 +0100

    Add RPC list example

---
+++ examples/rpc_list_formatting.py
@@ -0,0 +1,52 @@
+rpc_list = {
+    'eth': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'matic': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'arb': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'op': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'ftm': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'avax': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'metis': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'harmony': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'pulsechain': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    },
+    'bnb': {
+        'links': ["https://link1.com/",
+                  "https://link2.com/"
+                  ]
+    }
+}
